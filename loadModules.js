0d33a06fb5e1c94297b929dfd83b470bff5b0ecd
VolcanoCookies <franek.kg@gmail.com>
2020-01-03 15:08:18 +0100

    renamed to server all modules

---
+++ loadModules.js
@@ -0,0 +1,8 @@
+$(window).on('ready', function() {
+	$('#nav-placeholder').load(
+		'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/nav.html'
+	);
+	$('#footer-placeholder').load(
+		'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/footer.html'
+	);
+});
