5ebf959a34edd6a4dfcf83063258f8aa0f7eeeee
VolcanoCookies <franek.kg@gmail.com>
2020-01-04 02:33:31 +0100

    update

--- loadMembers.js
+++ loadMembers.js
@@ -3,44 +3,62 @@
 var membersContainer;
 var members;
 
-var background;
+var canvas;
+var ctx;
 
 var template;
 var jsonText;
+var placeholder;
 
-window.addEventListener(
-	'DOMContentLoaded',
-	function() {
-		membersContainer = document.getElementById('members-list');
-		background = document.getElementById('members-banner-span');
+var callsProcessed = 0;
 
-		getAsync(
-			'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/templates/memberTemplate.html',
-			receivedTemplate
-		);
+$(window).on('ready', function() {
+	membersContainer = document.getElementById('members-list');
+	background = document.getElementById('members-banner-span');
 
-		getAsync(
-			'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/members.json',
-			receivedList
-		);
-	},
-	false
-);
+	getAsync(
+		'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/templates/memberTemplate.html',
+		receivedTemplate
+	);
+
+	getAsync(
+		'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/members.json',
+		receivedList
+	);
+
+	getAsync(
+		'https://raw.githubusercontent.com/VolcanoCookies/Medusa-Network/master/images/profile%20pictures/Volcano.png',
+		receivedPlaceholderPicture
+	);
+
+	canvas = document.getElementById('canvas');
+
+	canvas.width = parent.innerWidth;
+	canvas.height = 400;
+	ctx = canvas.getContext('2d');
+
+	loadImages();
+});
 
 function receivedTemplate(response) {
 	template = response;
+	callsProcessed++;
 
-	if (jsonText != undefined) {
-		populateList();
-	}
+	if (callsProcessed == 3) populateList();
 }
 
 function receivedList(response) {
 	jsonText = response;
+	callsProcessed++;
 
-	if (template != undefined) {
-		populateList();
-	}
+	if (callsProcessed == 3) populateList();
+}
+
+function receivedPlaceholderPicture(response) {
+	placeholder = response;
+	callsProcessed++;
+
+	if (callsProcessed == 3) populateList();
 }
 
 function populateList() {
@@ -57,17 +75,7 @@ function populateList() {
 		var member = members.members[i];
 
 		createMember(member);
-
-		backgroundString += 'url("' + member.picture + '"), ';
 	}
-
-	backgroundString = backgroundString.substring(0, backgroundString.length - 2);
-
-	background.style.backgroundImage = backgroundString;
-
-	console.log(backgroundString);
-
-	console.log(background.style.backgroundImage);
 }
 
 function getAsync(url, callback) {
@@ -87,6 +95,10 @@ function createMember(member) {
 	var picture = member.picture;
 	var link = member.link;
 
+	if (picture == '') {
+		picture = placeholder;
+	}
+
 	var element = document.createElement('project');
 	var html = template.trim();
 	html = html.replace('member_name', name);
@@ -97,4 +109,66 @@ function createMember(member) {
 	element.innerHTML = html;
 
 	membersContainer.innerHTML += element.innerHTML;
+}
+
+//Canvas
+
+var imagesLoaded = [];
+
+function randomPattern(imgWidth, imgHeight, areaWidth, areaHeight) {
+	// either set a defined width/height for our images, or use the first one's
+	imgWidth = imgWidth || imagesLoaded[0].width;
+	imgHeight = imgHeight || imagesLoaded[0].height;
+	// restrict the randmoness size by using an areaWidth/Height
+	areaWidth = areaWidth || canvas.width;
+	areaHeight = areaHeight || canvas.height;
+
+	// create a buffer canvas
+	var patternCanvas = canvas.cloneNode(true);
+	var patternCtx = patternCanvas.getContext('2d');
+
+	patternCanvas.width = areaWidth;
+	patternCanvas.height = areaHeight;
+
+	var xloops = Math.ceil(areaWidth / imgWidth);
+	var yloops = Math.ceil(areaHeight / imgHeight);
+
+	for (var xpos = 0; xpos < xloops; xpos++) {
+		for (var ypos = 0; ypos < yloops; ypos++) {
+			var img = imagesLoaded[Math.floor(Math.random() * imagesLoaded.length)];
+			patternCtx.drawImage(
+				img,
+				xpos * imgWidth,
+				ypos * imgHeight,
+				imgWidth,
+				imgHeight
+			);
+		}
+	}
+
+	// create a pattern from this randomly created image
+	return patternCtx.createPattern(patternCanvas, 'repeat');
+}
+
+function loadImages() {
+	var imagesToLoad = 20;
+	for (var i = 0; i < imagesToLoad; i++) {
+		var image = new Image();
+		image.onload = function() {
+			imagesLoaded.push(this);
+			if (imagesLoaded.length === imagesToLoad) {
+				draw();
+			}
+		};
+		image.src = 'http://lorempixel.com/50/50?' + i;
+	}
+}
+
+function draw() {
+	//create the random pattern (should be moved out of the draw)
+	var patt = randomPattern(100, 100);
+	ctx.fillStyle = patt;
+	ctx.beginPath();
+	ctx.rect(0, 0, 2000, 700);
+	ctx.fill();
 }
